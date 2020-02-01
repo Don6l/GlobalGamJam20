0dcf55c28396e4c36d8438f6d295464e1dfb30a9
Don6l <donalmcclean@gmail.com>
2020-02-01 16:34:26 +0000

    Start game logic
    Begining to implement start game logic

---
+++ Assets/Scripts/GameMasterScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterScript : MonoBehaviour
+{
+    public static bool start_game = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_game = false;
+        Debug.Log(start_game);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!start_game)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButtonScript : MonoBehaviour
+{
+    public Button startButton;
+    public int number;
+    void Start()
+    {
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(StartGame);
+
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Clicked Start");
+        GameMasterScript.start_game = true;
+        
+    }
+
+}
